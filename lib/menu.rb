5b760a2087ca589f429fae214d20f5319cf6206d
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-20 18:07:14 -0600

    flushed out menu screen rb

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,5 @@
 
-
+require './lib/messages'
 
 class Menu
 
@@ -8,19 +8,45 @@ class Menu
   end
 
   def start_screen
-
-  end 
+    welcome_message
+    game_options_message
+    menu_selection = gets.chomp.downcase
+      if menu_selection == "p" || menu_selection == "play"
+        play
+      elsif menu_selection == "i" || menu_selection == "instructions"
+        instructions
+      elsif menu_selection == "q" || menu_selection == "quit"
+        quit
+      else
+        incorrect_selection_message
+        new_entry = gets.chomp.downcase
+        new_entry
+      end
+  end
 
   def play
+    computer_placement = ComputerShipPlacement.new
+    player_placement = PlayerShipPlacement.new 
 
   end
 
   def instructions
-
+    game_instructions_message
+    instructions_screen_entry = gets.chomp.downcase
+    if instructions_screen_entry == "p" || instructions_screen_entry == "play"
+      play
+    elsif instructions_screen_entry == "q" || instructions_screen_entry == "quit"
+      quit
+    else
+      incorrect_selection_message
+      new_entry = gets.chomp.downcase
+      new_entry
+    end
   end
 
   def quit
-
+    quit_message
+    exit
   end
 
 end
