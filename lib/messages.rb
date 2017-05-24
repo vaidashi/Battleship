d241334dab83e5700160a75527a51c02efb6f454
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-23 18:30:32 -0600

    some player board tests passing

--- lib/messages.rb
+++ lib/messages.rb
@@ -5,117 +5,117 @@ require 'pry'
 module Messages
 
   def welcome_message
-    puts "Welcome to BATTLESHIP"
+    p "Welcome to BATTLESHIP"
   end
 
   def game_options_message
-    puts "Would you like to (p)lay, read the (i)instructions, or (q)uit?"
+    p "Would you like to (p)lay, read the (i)instructions, or (q)uit?"
   end
 
   def incorrect_selection_message
-    puts "Sorry, I don't understand your selection. Please try again."
+    p "Sorry, I don't understand your selection. Please try again."
   end
 
   def game_instructions_message
-    puts "Battlship is a game where you and your opponent take turns attacking each other's ships\n"\
+    p "Battlship is a game where you and your opponent take turns attacking each other's ships\n"\
     "by correctly guessing their placement on the grid.\n"\
     "The first person to correctly guess and destroy all their opponent's ships wins."\
 
-    puts "I will be your opponent. I will place my ships and you will be prompted to place your ships."
-    puts "Ships cannot wrap around the board."
-    puts "Ships cannot overlap."
-    puts "Ships can be laid either horizontally or vertically."
-    puts "Coordinates must correspond to the first and last units of the ship. (IE: You can't place a two unit ship at 'A1 A3')"
+    p "I will be your opponent. I will place my ships and you will be prompted to place your ships."
+    p "Ships cannot wrap around the board."
+    p "Ships cannot overlap."
+    p "Ships can be laid either horizontally or vertically."
+    p "Coordinates must correspond to the first and last units of the ship. (IE: You can't place a two unit ship at 'A1 A3')"
   end
 
   def player_ship_placement_prompt
-    puts "I have laid out my ships on the grid."
-    puts "You now need to layout your two ships."
-    puts "The first is two units long and the second is three units long."
-    puts "The grid has A1 at the top left and D4 at the bottom right."
+    p "I have laid out my ships on the grid."
+    p "You now need to layout your two ships."
+    p "The first is two units long and the second is three units long."
+    p "The grid has A1 at the top left and D4 at the bottom right."
 
   end
 
   def player_two_element_ship_placement_message
-    puts "Enter the coordinates for the two-unit ship:"
-    puts "For example: A1 A2"
+    p "Enter the coordinates for the two-unit ship:"
+    p "For example: A1 A2"
 
   end
 
   def player_three_element_ship_placement_message
-    puts "Enter the coordinates for the three-unit ship:"
-    puts "For example: A1 A3"
+    p "Enter the coordinates for the three-unit ship:"
+    p "For example: A1 A3"
   end
 
   def enter_coordinates_to_attack_message
-    puts "Which position would you like to fire upon?"
-    puts "Make your decision by entering a coordinate you haven't chosen already and then press enter."
+    p "Which position would you like to fire upon?"
+    p "Make your decision by entering a coordinate you haven't chosen already and then press enter."
   end
 
   def player_board_message
-    puts "Player Board"
+    p "Player Board"
   end
 
   def computer_board_message
-    puts "Computer Board"
+    p "Computer Board"
   end
 
   def computer_to_player_hit_message
-    puts "Hit enemy ship!"
+    p "Hit enemy ship!"
   end
 
   def computer_to_player_sunk_two_element_ship_message
-    puts "You sunk my two-unit ship!"
+    p "You sunk my two-unit ship!"
   end
 
   def computer_to_player_sunk_three_element_ship_message
-    puts "You sunk my three-unit ship!"
+    p "You sunk my three-unit ship!"
   end
 
   def player_gets_hit_message
-    puts "Hit your ship!"
+    p "Hit your ship!"
   end
 
   def player_loses_two_element_ship_message
-    puts "Two-unit ship destroyed"
+    p "Two-unit ship destroyed"
   end
 
   def player_loses_three_element_ship_message
-    puts "Three-element ship destroyed"
+    p "Three-element ship destroyed"
   end
 
   def missed_message
-    puts "Missed"
+    p "Missed"
   end
 
   def player_wins_message
-    puts "You sunk my final battleship!"
-    puts "Congratulations, you win!"
+    p "You sunk my final battleship!"
+    p "Congratulations, you win!"
   end
 
   def player_loses_message
-    puts "I destroyed you final ship!"
-    puts "Sorry, you lose. Game over."
+    p "I destroyed you final ship!"
+    p "Sorry, you lose. Game over."
   end
 
   def out_of_range_ship_coordinate_message
-    puts "Sorry, those coordinates are out of range. Try again."
+    p "Sorry, those coordinates are out of range. Try again."
   end
 
   def invalid_ship_coordinate_message
-    puts "Sorry, those coordinates don't meet the position or size requirements. Try again."
+    p "Sorry, those coordinates don't meet the position or size requirements. Try again."
   end
 
   def overlapping_coordinate_message
-    puts "Sorry, those coordinates overlap your other ship. Try again."
+    p "Sorry, those coordinates overlap your other ship. Try again."
   end
 
   def already_fired_upon_message
-    puts "You have fired a shot here already. Try a different coordinate."
+    p "You have fired a shot here already. Try a different coordinate."
   end
 
   def quit_message
-    puts "You have quit the game."
+    p "You have quit the game."
   end
 
 end
--- test/player_board_test.rb
+++ test/player_board_test.rb
@@ -20,6 +20,37 @@ class PlayerBoardTest < Minitest::Test
     assert_instance_of Grid, player_grid
   end
 
+  def test_verification_of_coordinates
+    player = PlayerBoard.new
+    other_player = PlayerBoard.new
+
+    target = "A1"
+    verify  = player.verify_coord(target)
+
+    another_target = "A7"
+    verify_again = other_player.verify_coord(another_target)
+
+    assert verify
+    refute verify_again
+  end
+
+  def test_if_attempted_shots_get_archived
+    player = PlayerBoard.new
+    target = "A1"
+    verify  = player.verify_coord(target)
+
+    assert player.coordinates_targeted.length > 0
+  end
+
+  # def test_if_it_notifies_of_a_repeat_firing_coordinate
+  #   player = PlayerBoard.new
+  #   player.coordinates_targeted = [ "A1", "A2"]
+  #   target = "A1"
+  #   verify = player.verify_coord(target)
+  #
+  #   assert_equal already_fired_upon_message, verify
+  # end
+
 
 
 end
